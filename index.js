b077e44d8284e9427b8297b3f2b2bba354c41bac
DanielK <Danielkang13@gmail.com>
2022-08-07 11:23:15 -0700

    added function to add departments to database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const db = require('./config/config');
 const db_table = require('console.table');
+const { Console } = require('console');
 
 //view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 const options = ["VIEW all departments", "VIEW all roles", "VIEW all employees", "ADD a department", "ADD a role", "ADD an employee", "UPDATE an employee role", "Exit"]
@@ -33,35 +34,64 @@ function init() {
             case "VIEW all employees":
               viewEmployees();
               break;
+            case "ADD a department":
+              addDept();
+              break;
+
           }
     })
     .catch(err => console.log(err))
 }
 
 function viewDept() {
-    //TODO: query database on dept table
     db.query("SELECT * FROM departments;", function (err, results) {
         console.table(results);
         init();
     })
 }
-//job title, role id, the department that role belongs to, and the salary for that role
 function viewRoles() {
-    //TODO: query database on roles table
     const sqlViewRoles = `SELECT roles.id, roles.title, roles.salary, departments.name AS Department FROM roles JOIN departments ON roles.department_id = departments.id;`
     db.query(sqlViewRoles, function (err, results) {
         console.table(results);
         init();
     })
 }
-//including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 function viewEmployees() {
-    //TODO: query database on employee table
     const sqlViewEmp = `SELECT emp1.id, emp1.first_name AS First, emp1.last_name AS Last, roles.title AS Title, departments.name AS Department, roles.salary AS Salary, emp2.first_name AS Manager FROM employees AS emp1 LEFT JOIN employees AS emp2 ON emp1.manager_id = emp2.id LEFT JOIN roles ON emp1.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id`
     db.query(sqlViewEmp, function (err, results) {
         console.table(results);
         init();
     })
+}
+
+function addDept() {
+    //TODO: add a department to database get 
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Please Enter Name of Department: ",
+            name: "deptName"
+        }
+    ])
+    .then(results =>{
+        let correctStr = results.deptName.charAt(0).toUpperCase() + results.deptName.slice(1);
+        const sqlAdd = `INSERT INTO departments (name) VALUES ("${correctStr}");`
+        const sqlFind = `SELECT 1 FROM departments WHERE name = "${correctStr}";`
+        db.query(sqlFind, function (err, results) {
+            if (results.length === 1) {
+                console.log("Already in the database, Please choose a different title.")
+            } else {
+                db.query(sqlAdd, function (err, results) {
+                    if (err) {
+                        console.log(err, "Error on adding to database")
+                    }
+                })
+            }
+        })
+        console.log(`Added ${correctStr} Department to database!`);
+        init();
+    })
+    .catch(err => console.log(err));
 }
 
 init(); 
